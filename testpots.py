a362ce06775775b00a9400af343f43712a546b04
Ian Daniher <it.daniher@gmail.com>
2014-06-23 19:08:05 -0400

    set both loops to moderately small values

--- testpots.py
+++ testpots.py
@@ -3,4 +3,5 @@ import usb
 
 dev = usb.core.find(idVendor=0x0456,idProduct=0xcee2)
 dev.set_interface_altsetting(0,1)
-dev.ctrl_transfer(0x40|0x80, 0x1B, 0x5f7f, ord("a"), 4)
+dev.ctrl_transfer(0x40|0x80, 0x1B, 0x3040, ord("a"), 4)
+dev.ctrl_transfer(0x40|0x80, 0x1B, 0x3040, ord("b"), 4)
