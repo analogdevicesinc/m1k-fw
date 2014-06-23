8142bea8791801a39e1dbe151f9953766195dedf
Ian Daniher <it.daniher@gmail.com>
2014-06-23 10:51:35 -0400

    matches output from 'sigrok-cli --driver fx2lafw --continuous --config samplerate=1M -P i2c:sda=6:scl=7 | grep -B 3 DATA'

--- testi2c.py
+++ testi2c.py
@@ -3,6 +3,6 @@ import usb
 
 dev = usb.core.find(idVendor=0x0456,idProduct=0xcee2)
 dev.set_interface_altsetting(0,1)
-for i in range(255):
+for i in range(127):
 	if dev.ctrl_transfer(0x40|0x80, 0x5C, 0, i, 4)[0]:
-		print hex(i >> 1)
+		print hex(i)
