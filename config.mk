c31842bff21b7c375f11a61e3b266b377197aa2e
Dragos Bogdan <dragos.bogdan@analog.com>
2016-09-23 16:15:09 +0300

    config.mk: Change the PRJ_PATH from "asf" to "." This will prevent the removing of the source files when the solution is cleaned using AtmelStudio.

--- config.mk
+++ config.mk
@@ -1,4 +1,4 @@
-PRJ_PATH = asf
+PRJ_PATH = .
 ARCH = cortex-m3
 PART = sam3u2c
 
@@ -10,85 +10,85 @@ BUILD_DIR = build
 
 # List of C source files.
 CSRCS = \
-       ../src/main.c \
-	   common/boards/user_board/init.c                    \
-       common/services/clock/sam3u/sysclk.c               \
-       common/services/delay/sam/cycle_counter.c          \
-       common/services/sleepmgr/sam/sleepmgr.c            \
-       common/services/usb/class/vendor/device/udi_vendor.c \
-       common/services/usb/class/vendor/device/udi_vendor_desc.c \
-       common/services/usb/udc/udc.c                      \
-       common/utils/interrupt/interrupt_sam_nvic.c        \
-       sam/drivers/pio/pio.c                              \
-       sam/drivers/efc/efc.c                              \
-       sam/drivers/pio/pio_handler.c                      \
-       sam/drivers/tc/tc.c                                \
-       sam/drivers/pwm/pwm.c                              \
-       sam/drivers/twi/twi.c                              \
-       sam/drivers/wdt/wdt.c                              \
-       sam/drivers/pmc/pmc.c                              \
-       sam/drivers/pmc/sleep.c                            \
-       sam/drivers/usart/usart.c                          \
-       sam/drivers/udphs/udphs_device.c                   \
-       sam/services/flash_efc/flash_efc.c                  \
-       sam/utils/cmsis/sam3u/source/templates/exceptions.c \
-       sam/utils/cmsis/sam3u/source/templates/gcc/startup_sam3u.c \
-       sam/utils/cmsis/sam3u/source/templates/system_sam3u.c \
-       sam/utils/syscalls/gcc/syscalls.c
+	src/main.c                                                     \
+	asf/common/boards/user_board/init.c                            \
+	asf/common/services/clock/sam3u/sysclk.c                       \
+	asf/common/services/delay/sam/cycle_counter.c                  \
+	asf/common/services/sleepmgr/sam/sleepmgr.c                    \
+	asf/common/services/usb/class/vendor/device/udi_vendor.c       \
+	asf/common/services/usb/class/vendor/device/udi_vendor_desc.c  \
+	asf/common/services/usb/udc/udc.c                              \
+	asf/common/utils/interrupt/interrupt_sam_nvic.c                \
+	asf/sam/drivers/pio/pio.c                                      \
+	asf/sam/drivers/efc/efc.c                                      \
+	asf/sam/drivers/pio/pio_handler.c                              \
+	asf/sam/drivers/tc/tc.c                                        \
+	asf/sam/drivers/pwm/pwm.c                                      \
+	asf/sam/drivers/twi/twi.c                                      \
+	asf/sam/drivers/wdt/wdt.c                                      \
+	asf/sam/drivers/pmc/pmc.c                                      \
+	asf/sam/drivers/pmc/sleep.c                                    \
+	asf/sam/drivers/usart/usart.c                                  \
+	asf/sam/drivers/udphs/udphs_device.c                           \
+	asf/sam/services/flash_efc/flash_efc.c                         \
+	asf/sam/utils/cmsis/sam3u/source/templates/exceptions.c        \
+	asf/sam/utils/cmsis/sam3u/source/templates/gcc/startup_sam3u.c \
+	asf/sam/utils/cmsis/sam3u/source/templates/system_sam3u.c      \
+	asf/sam/utils/syscalls/gcc/syscalls.c
 
 # List of assembler source files.
 ASSRCS =
 
 # List of include paths.
 INC_PATH = \
-       ../src \
-       common/boards                                      \
-	   common/boards/user_board                           \
-       common/services/delay                              \
-       common/services/clock                              \
-       common/services/gpio                               \
-       common/services/ioport                             \
-       common/services/sleepmgr                           \
-       common/services/usb/udc                            \
-       common/services/usb                                \
-       common/services/usb/class/vendor                   \
-       common/services/usb/class/vendor/device            \
-       common/utils                                       \
-       sam/drivers/pio                                    \
-       sam/services/flash_efc                             \
-       sam/drivers/efc                                    \
-       sam/drivers/twi                                    \
-       sam/drivers/pmc                                    \
-       sam/drivers/wdt                                    \
-       sam/drivers/tc                                     \
-       sam/drivers/pwm                                    \
-       sam/drivers/udphs                                  \
-       sam/drivers/usart                                  \
-       sam/utils                                          \
-       sam/utils/cmsis/sam3u/include                      \
-       sam/utils/cmsis/sam3u/source/templates             \
-       sam/utils/header_files                             \
-       sam/utils/preprocessor                             \
-       thirdparty/CMSIS/Include                           \
-       thirdparty/CMSIS/Lib/GCC \
+	src                                         \
+	asf/common/boards                           \
+	asf/common/boards/user_board                \
+	asf/common/services/delay                   \
+	asf/common/services/clock                   \
+	asf/common/services/gpio                    \
+	asf/common/services/ioport                  \
+	asf/common/services/sleepmgr                \
+	asf/common/services/usb/udc                 \
+	asf/common/services/usb                     \
+	asf/common/services/usb/class/vendor        \
+	asf/common/services/usb/class/vendor/device \
+	asf/common/utils                            \
+	asf/sam/drivers/pio                         \
+	asf/sam/services/flash_efc                  \
+	asf/sam/drivers/efc                         \
+	asf/sam/drivers/twi                         \
+	asf/sam/drivers/pmc                         \
+	asf/sam/drivers/wdt                         \
+	asf/sam/drivers/tc                          \
+	asf/sam/drivers/pwm                         \
+	asf/sam/drivers/udphs                       \
+	asf/sam/drivers/usart                       \
+	asf/sam/utils                               \
+	asf/sam/utils/cmsis/sam3u/include           \
+	asf/sam/utils/cmsis/sam3u/source/templates  \
+	asf/sam/utils/header_files                  \
+	asf/sam/utils/preprocessor                  \
+	asf/thirdparty/CMSIS/Include                \
+	asf/thirdparty/CMSIS/Lib/GCC
 
 # Additional search paths for libraries.
-LIB_PATH =  \
-       thirdparty/CMSIS/Lib/GCC
+LIB_PATH = \
+       asf/thirdparty/CMSIS/Lib/GCC
 
 # List of libraries to use during linking.
 LIBS =  \
-       arm_cortexM3l_math                                 \
+       arm_cortexM3l_math \
        m
 
 # Path relative to top level directory pointing to a linker script.
-LINKER_SCRIPT_FLASH = ../scripts/flash.ld
+LINKER_SCRIPT_FLASH = scripts/flash.ld
 
 # Path relative to top level directory pointing to a linker script.
 DEBUG_SCRIPT_FLASH =
 
 # Project type parameter: all, sram or flash
-PROJECT_TYPE        = flash
+PROJECT_TYPE = flash
 
 # Additional options for debugging. By default the common Makefile.in will
 # add -g3.
@@ -117,10 +117,10 @@ CFLAGS = -fstack-usage -Wno-attributes
 #   BOARD      Target board in use, see boards/board.h for a list.
 #   EXT_BOARD  Optional extension board in use, see boards/board.h for a list.
 CPPFLAGS = \
-       -D ARM_MATH_CM3=true                               \
-       -D BOARD=USER_BOARD                                \
-       -D UDD_ENABLE                                      \
-       -D __SAM3U2C__                                     \
+       -D ARM_MATH_CM3=true \
+       -D BOARD=USER_BOARD  \
+       -D UDD_ENABLE        \
+       -D __SAM3U2C__       \
        -D printf=iprintf
 
 # Extra flags to use when linking
